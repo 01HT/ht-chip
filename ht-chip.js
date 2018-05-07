8b2bc27d6b40a4cc252e7bd45ab28d0431f5b3bf
Maksim Yurkov <yurkov.maksim.sergeevich@gmail.com>
2018-05-07 11:15:41 +0300

    Update ht-chip.js

--- ht-chip.js
+++ ht-chip.js
@@ -36,11 +36,6 @@ class HTElementsChip extends LitElement {
           flex-direction: row;
           padding: 0 12px 0 12px;
           align-items: center;
-          box-shadow: ${
-            shadow
-              ? "0 2px 2px 0 rgba(0, 0, 0, 0.14), 0 1px 5px 0 rgba(0, 0, 0, 0.12), 0 3px 1px -2px rgba(0, 0, 0, 0.2);"
-              : "none"
-          };
         }
 
         #container:hover {
@@ -88,7 +83,11 @@ class HTElementsChip extends LitElement {
           </defs>
         </svg>
       </iron-iconset-svg>
-      <div id="container">
+      <div id="container" style=${
+        shadow
+          ? "box-shadow: 0 2px 2px 0 rgba(0, 0, 0, 0.14), 0 1px 5px 0 rgba(0, 0, 0, 0.12), 0 3px 1px -2px rgba(0, 0, 0, 0.2);"
+          : ""
+      }>
         <slot name="avatar"></slot>
         <div id="label">${label}</div>
         <iron-icon icon="ht-chip-icons:cancel" hidden?=${
