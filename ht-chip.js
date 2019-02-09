8ffdb49649cfb2594f91f3fba2f057a587c88c25
maksimyurkov <yurkov.maksim.sergeevich@gmail.com>
2019-02-09 12:27:19 +0300

    new lit-element changes

--- ht-chip.js
+++ ht-chip.js
@@ -1,109 +1,110 @@
 "use strict";
-import { LitElement, html } from "@polymer/lit-element";
+import { LitElement, html, css } from "lit-element";
 import "@polymer/iron-iconset-svg/iron-iconset-svg.js";
 import "@polymer/iron-icon";
 
 class HTElementsChip extends LitElement {
+  static styles = css`<style>
+    :host {
+      display: block;
+      position:relative;
+      box-sizing:border-box;
+    }
+
+    iron-icon {
+      --iron-icon-height: 18px;
+      --iron-icon-width: 18px;
+      position: relative;
+      color:#A6A6A6;
+      border-radius: 50%;
+      cursor: pointer;
+      margin-left:8px;
+    }
+
+    #container {
+      max-width: 276px;
+      background:#fff;
+      font-size: 13px;
+      color: rgba(0, 0, 0, 0.87);
+      background: #fff;
+      border-radius: 16px;
+      height:32px;
+      position:relative;
+      display: flex;
+      justify-content: space-between;
+      flex-direction: row;
+      padding: 0 12px 0 12px;
+      align-items: center;
+    }
+
+    #container:hover {
+      box-shadow: 0 2px 2px 0 rgba(0, 0, 0, 0.14),
+      0 1px 5px 0 rgba(0, 0, 0, 0.12),
+      0 3px 1px -2px rgba(0, 0, 0, 0.2);
+    }
+
+    #label {
+      white-space: nowrap;
+      text-overflow: ellipsis;
+      overflow: hidden;
+    }
+
+      ::slotted(div) {
+      display:flex;
+      align-items:center; 
+      justify-content:center;
+      --iron-icon-height: 18px; 
+      --iron-icon-width: 18px;
+      }
+
+    :host([full]) #container {
+      padding: 0 12px 0 0;
+    }
+
+    :host([full]) ::slotted(div) {
+      margin-right: 8px;
+      width:32px;
+      height:32px;
+    }
+
+    :host([image]) #container {
+      padding: 0 12px 0 8px;
+    }
+
+    :host([image]) ::slotted(div) {
+        display: flex;
+        width:18px;
+        height:18px;
+        margin-right:8px;
+    }
+
+    :host([icon]) #container {
+      padding: 0 12px 0 4px;
+    }
+
+    :host([icon]) ::slotted(div)  {
+      border-radius: 50%;
+      background: #A6A6A6;
+      color: #ffffff;
+      font-weight: 700;
+      font-size: 16px;
+      margin-right:8px;
+      width:24px;
+      height:24px;
+    }
+
+    :host([close]) #container { 
+      padding-right: 8px;
+    }
+
+    [hidden] {
+      display:none;
+    }
+  </style>`;
+
   render() {
     const { label, close, shadow } = this;
     return html`
-      <style>
-        :host {
-          display: block;
-          position:relative;
-          box-sizing:border-box;
-        }
-
-        iron-icon {
-          --iron-icon-height: 18px;
-          --iron-icon-width: 18px;
-          position: relative;
-          color:#A6A6A6;
-          border-radius: 50%;
-          cursor: pointer;
-          margin-left:8px;
-        }
-
-        #container {
-          max-width: 276px;
-          background:#fff;
-          font-size: 13px;
-          color: rgba(0, 0, 0, 0.87);
-          background: #fff;
-          border-radius: 16px;
-          height:32px;
-          position:relative;
-          display: flex;
-          justify-content: space-between;
-          flex-direction: row;
-          padding: 0 12px 0 12px;
-          align-items: center;
-        }
-
-        #container:hover {
-          box-shadow: 0 2px 2px 0 rgba(0, 0, 0, 0.14),
-          0 1px 5px 0 rgba(0, 0, 0, 0.12),
-          0 3px 1px -2px rgba(0, 0, 0, 0.2);
-        }
-
-        #label {
-          white-space: nowrap;
-          text-overflow: ellipsis;
-          overflow: hidden;
-        }
-
-         ::slotted(div) {
-          display:flex;
-          align-items:center; 
-          justify-content:center;
-          --iron-icon-height: 18px; 
-          --iron-icon-width: 18px;
-         }
-
-        :host([full]) #container {
-          padding: 0 12px 0 0;
-        }
-
-        :host([full]) ::slotted(div) {
-          margin-right: 8px;
-          width:32px;
-          height:32px;
-        }
-
-        :host([image]) #container {
-          padding: 0 12px 0 8px;
-        }
-
-        :host([image]) ::slotted(div) {
-            display: flex;
-            width:18px;
-            height:18px;
-            margin-right:8px;
-        }
-
-        :host([icon]) #container {
-          padding: 0 12px 0 4px;
-        }
-
-        :host([icon]) ::slotted(div)  {
-          border-radius: 50%;
-          background: #A6A6A6;
-          color: #ffffff;
-          font-weight: 700;
-          font-size: 16px;
-          margin-right:8px;
-          width:24px;
-          height:24px;
-        }
-
-        :host([close]) #container { 
-          padding-right: 8px;
-        }
-
-        [hidden] {
-          display:none;
-        }
-      </style>
       <iron-iconset-svg size="24" name="ht-chip-icons">
         <svg>
           <defs>
@@ -118,15 +119,11 @@ class HTElementsChip extends LitElement {
       }">
         <slot name="avatar"></slot>
         <div id="label">${label}</div>
-        <iron-icon icon="ht-chip-icons:cancel" ?hidden=${!close} @click=${e => {
-      this._close();
-    }}></iron-icon>
+        <iron-icon icon="ht-chip-icons:cancel" ?hidden="${!close}" @click="${
+      this._close
+    }"></iron-icon>
       </div>
 `;
-  }
-
-  static get is() {
-    return "ht-chip";
   }
 
   static get properties() {
@@ -148,4 +145,4 @@ class HTElementsChip extends LitElement {
   }
 }
 
-customElements.define(HTElementsChip.is, HTElementsChip);
+customElements.define("ht-chip", HTElementsChip);
